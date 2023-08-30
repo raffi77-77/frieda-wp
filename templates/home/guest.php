2ed8489b5ed006bc0108aa0c269105642673c421
stripearmy <mkrtchyan.ars@gmail.com>
2023-08-30 23:20:28 +0400

    guest.php updated

--- templates/home/guest.php
+++ templates/home/guest.php
@@ -1,225 +1,170 @@
 
-<div class="banner-section">
-	<div class="container">
-		<div class="banner-wrap">
-			<div class="banner-content">
-				<h4><?= get_field('hero_section_tagline');?></h4>
-				<h1 class="title"><?= get_field('hero_section_title');?></h1>
-				<p class="paragraph"><?= get_field('hero_section_content');?></p>
-
-				<?php
-					$button = get_field('hero_section_button_section');
-					$button1 = $button['button_1'];
-					$button2 = $button['button_2'];
-					if ($button1 || $button2) { 
-				?>
-					<div class="btn-wrap">
-						<?php if ($button1) { ?>
-							<a href="<?= $button1['url']; ?>" class="dark-btn" target="<?= $button1['target']; ?>"><?= $button1['title']; ?></a>
-						<?php } ?>
-						<?php if ($button2) { ?>
-							<a href="<?= $button2['url'];?>" class="normal-btn" target="<?= $button2['target'];?>"><?= $button2['title'];?></a>
-						<?php } ?>
-					</div>
-				<?php } ?>
-			</div>
-			<div class="banner-image">
-				<img src="<?= get_field('hero_section_image');?>"/>
-			</div>		
-		</div>
-
-		<!-- Info Section -->
-		<!-- <div class="info-section">
-
-			<?php
-				$infoList = get_field('home_info_list');
-				foreach ($infoList as $key => $info) {
-			?>
-				<div class="info-block">
-					<div class="info-icon">
-						<img src="<?= $info['image'];?>"/>
-					</div>
-					<div class="info-info">
-						<h2><?= $info['title'];?></h2>
-						<p class="paragraph"><?= $info['content'];?> </p>
-					</div>
-				</div>
-			<?php } ?>
- 
-		</div> -->
-	</div>
+<div class="header-padding homepage flex flex-col justify-start align-stretch">
+    <div class="homepage-banner" style="background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/homepage/homepage-banner.jpg)">
+        <div class="container">
+            <div class="homepage-banner-content flex flex-col justify-start align-stretch">
+                <h3 class="roboto">Bis zu 100% von deiner Krankenkasse erstattet</h3>
+                <h2 class="gilda">Effektive Unterstützung zur Bewältigung von Wechseljahresbeschwerden</h2>
+                <p class="roboto">Mit unseren speziell entwickelten digitalen Angeboten
+                    unterstützen wir dich dabei, die Herausforderungen der
+                    Wechseljahre selbstbewusst zu meistern. Dabei übernimmt
+                    deine Krankenkasse die Kosten bis zu 100%.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="homepage-promo-wrap">
+        <div class="container">
+            <div class="homepage-promo flex justify-start align-start">
+                <div class="grow-1 w-full">
+                    <div class="homepage-promo-block flex justify-start align-start">
+                        <div class="homepage-promo-block-icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/homepage/promo-icon-1.png" alt="">
+                        </div>
+                        <div class="homepage-promo-block-content flex flex-col justify-start align-stretch">
+                            <h2 class="gilda">Von Krankenkassen zertifiziert</h2>
+                            <p class="roboto">Unsere Kurse sind von allen Deutschen Gesetzlichen Krankenversicherungen anerkannt.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="grow-1 w-full">
+                    <div class="homepage-promo-block flex justify-start align-start">
+                        <div class="homepage-promo-block-icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/homepage/promo-icon-2.png" alt="">
+                        </div>
+                        <div class="homepage-promo-block-content flex flex-col justify-start align-stretch">
+                            <h2 class="gilda">Wissenschaftlich fundiert</h2>
+                            <p class="roboto">Unsere Kurse basieren auf neuster Evidenz und wir führen Wirksamkeitsstudien zu all unseren Kursen durch.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="grow-1 w-full">
+                    <div class="homepage-promo-block flex justify-start align-start">
+                        <div class="homepage-promo-block-icon">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/homepage/promo-icon-3.png" alt="">
+                        </div>
+                        <div class="homepage-promo-block-content flex flex-col justify-start align-stretch">
+                            <h2 class="gilda">Datensicherheit & DSGVO</h2>
+                            <p class="roboto">Wir erheben keinerlei personenbezogenen Daten und schützen deine Privatsphäre bei jedem Schritt.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="homepage-list-wrap flex flex-col justify-start align-stretch">
+        <div class="container flex flex-col justify-start align-stretch">
+            <h2 class="homepage-list-wrap-title gilda">Entdecke deine Frieda-Kurse</h2>
+        </div>
+        <div class="container">
+            <div class="homepage-list-container flex justify-start align-center">
+                <div class="homepage-list grow-1 flex flex-col justify-start align-stretch">
+                    <div class="homepage-list-item roboto"><span>✔</span> Optimal auf die Wechseljahre abgestimmte Online-Kurse</div>
+                    <div class="homepage-list-item roboto"><span>✔</span> Entwickelt von Expertinnen aus Medizin, Psychologie und Ernährungswissenschaft</div>
+                    <div class="homepage-list-item roboto"><span>✔</span> Flexible 8-Wochen-Kurse – erlebbar via Laptop oder Mobiltelefon</div>
+                    <div class="homepage-list-item roboto"><span>✔</span> Geführte Meditationen, individuelles Coaching, effektive Atemübungen</div>
+                </div>
+                <div class="homepage-slider-wrap grow-1">
+                    <div class="homepage-slider flex">
+                        <div class="homepage-slider-item">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/homepage/homepage-slider-1.png" alt="">
+                        </div>
+                        <div class="homepage-slider-item">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/homepage/homepage-slider-2.png" alt="">
+                        </div>
+                        <div class="homepage-slider-item">
+                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/homepage/homepage-slider-3.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="homepage-steps-wrap">
+        <div class="container">
+            <div class="flex flex-col justify-start align-stretch">
+                <h2 class="homepage-steps-wrap-title">In drei Schritten zu deinem erstatteten Kurs</h2>
+                <div class="homepage-steps flex justify-center align-start">
+                    <div class="homepage-steps-item flex flex-col justify-start align-center">
+                        <div class="homepage-steps-count gilda">1</div>
+                        <div class="homepage-steps-text roboto">Kurs auswählen und buchen</div>
+                    </div>
+                    <div class="homepage-steps-item flex flex-col justify-start align-center">
+                        <div class="homepage-steps-count gilda">2</div>
+                        <div class="homepage-steps-text roboto">Am Kurs teilnehmen</div>
+                    </div>
+                    <div class="homepage-steps-item flex flex-col justify-start align-center">
+                        <div class="homepage-steps-count gilda">3</div>
+                        <div class="homepage-steps-text roboto">Teilnahmebestätigung erhalten und bei deiner Krankenkasse einreichen</div>
+                    </div>
+                </div>
+                <div class="flex justify-center align-center">
+                    <a href="#" class="homepage-button roboto">So einfach funktioniert die Erstattung</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="homepage-experts-wrap">
+        <div>
+            <div class="container flex flex-col justify-start align-stretch">
+                <h2 class="homepage-experts-wrap-title gilda">Unsere Experten an deiner Seite</h2>
+                <p class="homepage-experts-wrap-subtitle roboto">Erfahrene Fachleute aus Medizin und Verhaltenswissenschaften haben unsere Kurse<br/> mitentwickelt, um dir die bestmögliche Unterstützung zu bieten.</p>
+            </div>
+        </div>
+        <div class="homepage-experts-bg">
+            <div class="container">
+                <div class="homepage-experts flex justify-start align-center">
+                    <div class="homepage-experts-item">
+                        <img class="homepage-experts-item-img" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/homepage/person-1.png" alt="">
+                        <div class="homepage-experts-item-text">
+                            <h2 class="gilda">Dr. Kai Schulze-Wundling</h2>
+                            <p class="roboto">Medizinwissenschaftler und Mit-Gründer von Frieda</p>
+                        </div>
+                    </div>
+                    <div class="homepage-experts-item">
+                        <img class="homepage-experts-item-img" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/homepage/person-2.jpeg" alt="">
+                        <div class="homepage-experts-item-text">
+                            <h2 class="gilda">Johanna Zeder</h2>
+                            <p class="roboto">Psychologin</p>
+                        </div>
+                    </div>
+                    <div class="homepage-experts-item">
+                        <img class="homepage-experts-item-img" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/homepage/person-3.png" alt="">
+                        <div class="homepage-experts-item-text">
+                            <h2 class="gilda">Dr. Michael Müller</h2>
+                            <p class="roboto">Facharzt für Innere Medizin und Endokrinologie</p>
+                        </div>
+                    </div>
+                    <div class="homepage-experts-item">
+                        <img class="homepage-experts-item-img" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/homepage/person-4.jpeg" alt="">
+                        <div class="homepage-experts-item-text">
+                            <h2 class="gilda">Sophia Weikel</h2>
+                            <p class="roboto">Psychotherapeutin</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="homepage-social-wrap">
+        <div class="container">
+            <div class="flex flex-col justify-start align-stretch">
+                <h2 class="homepage-social-wrap">Folge uns auf Instagram und hole dir tägliche Inspirationen</h2>
+                <div class="flex justify-between align-center">
+                    <span class="homepage-social-name">frieda.health</span>
+                    <div class="tooltip-holder tooltip-holder__bottom">
+                        <a class="instagram-button" href="https://www.instagram.com/frieda.health/" target="_blank">
+                            <svg width="16" height="16" viewBox="0 0 24 24">
+                                <path d="M13.156.008c2.223.004 2.677.021 3.786.072 1.277.058 2.15.26 2.912.557a5.88 5.88 0 012.125 1.384 5.88 5.88 0 011.384 2.125c.296.763.499 1.635.557 2.912.055 1.194.07 1.63.072 4.331v1.23c-.002 2.701-.017 3.137-.072 4.33-.058 1.278-.26 2.15-.557 2.913a5.88 5.88 0 01-1.384 2.125 5.88 5.88 0 01-2.125 1.383c-.763.297-1.635.5-2.912.558-1.194.054-1.63.07-4.331.072h-1.23c-2.701-.002-3.137-.018-4.33-.072-1.278-.058-2.15-.261-2.913-.558a5.88 5.88 0 01-2.125-1.383A5.88 5.88 0 01.63 19.862c-.297-.763-.5-1.635-.558-2.912-.05-1.11-.068-1.564-.071-3.786v-2.32C.004 8.62.02 8.167.072 7.058.13 5.78.333 4.908.63 4.146A5.88 5.88 0 012.013 2.02 5.88 5.88 0 014.138.637C4.901.341 5.773.138 7.05.08 8.16.03 8.614.012 10.836.008zm-.28 2.161h-1.76c-2.408.003-2.829.018-3.967.07-1.17.053-1.805.249-2.228.413-.56.218-.96.478-1.38.897-.419.42-.679.82-.897 1.38-.164.422-.36 1.058-.413 2.227-.052 1.139-.067 1.56-.07 3.968v1.76c.003 2.408.018 2.829.07 3.967.054 1.17.25 1.805.413 2.228.218.56.478.96.898 1.38.42.419.82.679 1.38.897.422.164 1.057.36 2.227.413 1.096.05 1.527.065 3.708.069h2.279c2.18-.004 2.612-.02 3.708-.07 1.17-.053 1.805-.248 2.227-.412.56-.218.96-.478 1.38-.898.42-.42.68-.82.897-1.38.164-.422.36-1.057.413-2.227.05-1.096.066-1.527.07-3.708v-2.279c-.004-2.18-.02-2.611-.07-3.708-.053-1.17-.249-1.805-.413-2.227a3.716 3.716 0 00-.897-1.38c-.42-.42-.82-.68-1.38-.897-.422-.164-1.058-.36-2.227-.413-1.139-.052-1.56-.067-3.968-.07zm-.88 3.675a6.16 6.16 0 110 12.32 6.16 6.16 0 010-12.32zm0 2.161a3.999 3.999 0 100 7.998 3.999 3.999 0 000-7.998zM18.4 4.161a1.44 1.44 0 110 2.879 1.44 1.44 0 010-2.88z" fill="current" fill-rule="evenodd"></path>
+                            </svg>
+                            <span>Abonnieren</span>
+                        </a>
+                        <div class="tooltip tooltip__bottom tooltip__right">
+                            <span>Dadurch wird Instagram in einem neuen Tab geöffnet.</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-<div class="hows-it-works">
-	<div class="container">
-		<div class="heading">
-			<h2 class="title"><?= get_field('hiw_section_title');?></h2>
-		</div>
-		<div class="zigzag-wrap">
-			<?php
-				$hiwList = get_field('hiw_list');
-				foreach ($hiwList as $key => $info) {
-			?> 
-				<div class="zigzag-block">
-					<div class="zigzag-content">
-						<h3><?= $info['title']; ?></h3>
-						<p class="paragraph"><?= $info['content']; ?></p>
-						<a href="<?= $info['button']['url'];?>" class="dark-btn" target="<?= $info['button']['target'];?>"><?= $info['button']['title'];?></a>
-					</div>
-					<div class="zigzag-image">
-						<img src="<?= $info['image']; ?>"/>
-					</div>
-				</div>
-			<?php } ?>
-
-		</div>
-	</div>
-</div>
-<div class="calc-wrap">
-	<div class="container">
-		<h2><?= get_field('calculator_heading'); ?></h2>
-		<div class="calc-content">
-			<div class="clac-desc">
-				<p><?= get_field('calculator_description'); ?></p>
-			</div>
-			<div class="clac-info"> 
-				<?php $insuranceCompanies = get_field('choose_insurance_company', 'options'); ?>
-				<select name="insuranceCompany" id="insuranceCompany" required>
-					<option value="" hidden><?= get_field('calculator_select_text'); ?></option>
-					<?php foreach ($insuranceCompanies as $key => $company) { ?>
-						<option value="<?= $company['percentage']; ?>"><?= $company['company_name']; ?></option>
-					<?php } ?>
-				</select>
-				<div class="innerinfo">
-					<div class="price">
-						<h3><?= get_field('course_fee'); ?></h3>
-						<p>
-							<span class="regularPrice"><?= get_post_meta(117, '_regular_price', true); ?>.00</span>€
-						</p>
-					</div>
-					<div class="price discount">
-						<h3><?= get_field('calculator_coverage_'); ?></h3>
-
-						<!-- <h3>Durch deine Krankenkasse erstattet:</h3> -->
-						<p>
-							<span class="discountPercentage">0</span>€
-						</p>
-					</div>
-					<div class="price total-price">
-						<h3><?= get_field('calculator_contribution'); ?></h3>
-						<p>
-							<span class="pendingPrice"><?= get_post_meta(117, '_regular_price', true); ?>.00</span>€
-						</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-<div class="banner-section infowrap-section">
-	<div class="container">
-		<!-- <div class="banner-wrap">
-			<div class="banner-content">
-				<h4><?= get_field('hero_section_tagline');?></h4>
-				<h1 class="title"><?= get_field('hero_section_title');?></h1>
-				<p class="paragraph"><?= get_field('hero_section_content');?></p>
-
-				<?php
-					$button = get_field('hero_section_button_section');
-					$button1 = $button['button_1'];
-					$button2 = $button['button_2'];
-					if ($button1 || $button2) { 
-				?>
-					<div class="btn-wrap">
-						<?php if ($button1) { ?>
-							<a href="<?= $button1['url']; ?>" class="dark-btn" target="<?= $button1['target']; ?>"><?= $button1['title']; ?></a>
-						<?php } ?>
-						<?php if ($button2) { ?>
-							<a href="<?= $button2['url'];?>" class="normal-btn" target="<?= $button2['target'];?>"><?= $button2['title'];?></a>
-						<?php } ?>
-					</div>
-				<?php } ?>
-			</div>
-			<div class="banner-image">
-				<img src="<?= get_field('hero_section_image');?>"/>
-			</div>		
-		</div> -->
-
-		<!-- Info Section -->
-		<div class="info-section">
-
-			<?php
-				$infoList = get_field('home_info_list');
-				foreach ($infoList as $key => $info) {
-			?>
-				<div class="info-block">
-					<div class="info-icon">
-						<img src="<?= $info['image'];?>"/>
-					</div>
-					<div class="info-info">
-						<h2><?= $info['title'];?></h2>
-						<p class="paragraph"><?= $info['content'];?> </p>
-					</div>
-				</div>
-			<?php } ?>
- 
-		</div>
-	</div>
-</div>
-<div class="whatdoing-section">
-	<div class="container">
-		<div class="whatdoing-wrap">
-			<div class="whatdoing-image">
-				<img src="<?= get_field('wds_image');?>"/> 
-				<!-- <iframe width="602" height="557" src="https://www.youtube.com/embed/uWQ_8CtvzoY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> -->
-			</div>
-			<div class="whatdoing-content">
-				<h3 class="sub-heading"><?= get_field('wds_title');?></h3>
-				<p class="paragraph"><?= get_field('wds_content');?></p>
-				<?php $button = get_field('wds_button'); ?>
-				<a href="<?= $button['url'];?>" class="white-btn" target="<?= $button['target'];?>"><?= $button['title'];?></a>
-			</div>
-		</div>
-	</div>
-</div>
-<div class="learnmore-wrap">
-	<div class="container" style="background-image: url('<?= get_field('lms_image');?>');">
-			<div class="learnmore-content">
-				<h3 class="sub-heading"><?= get_field('lms_title');?></h3>
-				<p><?= get_field('lms_title_content');?></p>
-				<?php $button = get_field('lms_button'); ?>
-				<a href="<?= $button['url'];?>" class="white-btn" target="<?= $button['target'];?>"><?= $button['title'];?></a>
-			</div>	
-		</div>
-	</div>
-</div>
-<div class="support-wrap">
-	<div class="container">
-		<div class="heading">
-			<!-- <h2 class="title"><//?= get_field('h_ss_section_title');?></h2> -->
-		</div>
-		<div class="support-blocks">
-
-			<?php
-				$hSsList = get_field('h_ss_list');
-				foreach ($hSsList as $key => $list) {
-			?> 
-				<div class="support-content">
-					<div class="icon">
-						<img src="<?= $list['image']; ?>"/>
-					</div>
-					<h4><?= $list['title']; ?></h4>
-					<p class="paragraph"><?= $list['content']; ?></p>
-				</div>
-			<?php } ?>
-			 
-		</div>
-	</div>
-</div>
-<!-- 
-<div class="video-section">
-   <div class="container">
-		<video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
-			<source src="https://flixels.s3.amazonaws.com/flixel/ct08dlpwq6eqp8yp1r6v.hd.mp4" type="video/mp4">
-		</video>
-   </div>
-</div> -->
